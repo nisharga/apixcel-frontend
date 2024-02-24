86f61fce0cde8100fd6f48d8795230da2455a015
nex-nisharga <kabir@nexlent.com>
2024-02-24 11:43:57 +0600

    logout api added

---
+++ src/app/api/logout/route.tsx
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  const response = NextResponse.json(
+    { error: null, msg: "Logout success" },
+    {
+      status: 200,
+    }
+  );
+
+  response.cookies.delete("token");
+
+  return response;
+};
