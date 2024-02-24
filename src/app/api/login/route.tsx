6c5042938c692149afde4b49937905a0c43ee960
nex-nisharga <kabir@nexlent.com>
2024-02-24 11:33:21 +0600

    user login added

---
+++ src/app/api/login/route.tsx
@@ -0,0 +1,51 @@
+ 
+import { ConnectDB } from "@/config/dbconnect";
+import { GenerateToken } from "@/lib/Token";
+import { UserModel } from "@/lib/User.Model";
+import { NextResponse } from "next/server";
+
+ConnectDB();
+
+export const POST = async (request: Request) => {
+  const { email, password } = await request.json();
+
+  const existUser = await UserModel.findOne({ email });
+  if (!existUser) {
+    return NextResponse.json(
+      { msg: null, error: "User Not Exist" },
+      {
+        status: 400,
+      }
+    );
+    return;
+  }
+
+  const isMatch = await existUser.ConfirmPassword(password);
+  if (!isMatch) {
+    return NextResponse.json(
+      { msg: null, error: "Invalid Credentails" },
+      {
+        status: 400,
+      }
+    );
+    return;
+  }
+
+  // token
+
+  const token = await GenerateToken(existUser);
+
+  const reponse = NextResponse.json(
+    { error: null, msg: "User Login Successfully" },
+    {
+      status: 200,
+    }
+  );
+
+  reponse.cookies.set("token", token, {
+    httpOnly: true,
+    secure: true,
+  });
+
+  return reponse;
+};
